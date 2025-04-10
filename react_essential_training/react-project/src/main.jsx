7d83c9d6313ae4f951d9011f893a7cc9e9e03a80
Luke Stringer <luke97stringer@gmail.com>
2025-04-10 16:52:46 +1000

    arrays destructuring with ecma script

--- react_essential_training/react-project/src/main.jsx
+++ react_essential_training/react-project/src/main.jsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+//const animals = ["dog", "cat", "sea cow"];
+//console.log(animals[0]);
+//replaced with
+const [firstAnimal, , third] = ["dog", "cat", "sea cow"]; //skip over with comma
+console.log(firstAnimal, third);
+
 createRoot(document.getElementById("root")).render(<App />);

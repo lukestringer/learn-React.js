81f3cc763b419fa8cfb3d9d1ae7d4321a63544f1
Luke Stringer <luke97stringer@gmail.com>
2025-04-10 16:53:02 +1000

    remove array destructuring lesson

--- react_essential_training/react-project/src/main.jsx
+++ react_essential_training/react-project/src/main.jsx
@@ -2,10 +2,4 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-//const animals = ["dog", "cat", "sea cow"];
-//console.log(animals[0]);
-//replaced with
-const [firstAnimal, , third] = ["dog", "cat", "sea cow"]; //skip over with comma
-console.log(firstAnimal, third);
-
 createRoot(document.getElementById("root")).render(<App />);

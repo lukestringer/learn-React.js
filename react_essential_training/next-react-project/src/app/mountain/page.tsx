f5ce8df1fd1ed84f06bfb386c4309280ca49dc94
Luke Stringer <luke97stringer@gmail.com>
2025-04-14 17:17:13 +1000

    create new next.js app, delete default app, add /mountain route

---
+++ react_essential_training/next-react-project/src/app/mountain/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main>
+            <h1>Lift Status Info</h1>
+            {/* npm run dev  then go to localhost:3000/mountain to see this page*/}
+        </main>
+    );
+}

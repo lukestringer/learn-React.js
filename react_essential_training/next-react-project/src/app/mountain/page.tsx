be81c010a1942bb9bd5fd12fd3a550b0a3afac9c
Luke Stringer <luke97stringer@gmail.com>
2025-04-15 17:00:17 +1000

    fetching data with a server component

--- react_essential_training/next-react-project/src/app/mountain/page.tsx
+++ react_essential_training/next-react-project/src/app/mountain/page.tsx
@@ -1,8 +1,87 @@
-export default function Page() {
-    return (
-        <main>
-            <h1>Lift Status Info</h1>
-            {/* npm run dev  then go to localhost:3000/mountain to see this page*/}
-        </main>
-    );
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+} from "react";
+
+async function getData() {
+  const res = await fetch("https://snowtooth-api-rest.fly.dev");
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+  return (
+    <main>
+      <h1>Lift Status Info</h1>
+      {/* npm run dev  then go to localhost:3000/mountain to see this page*/}
+      {/* <div>{JSON.stringify(data)}</div> */}
+      <table>
+        <thead>
+          <tr>
+            <th>Lift Name</th>
+            <th>Current Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* All the type info added with Quick Fix (problem because this is TypeScript not JS) */}
+          {data.map(
+            (lift: {
+              id: Key | null | undefined;
+              name:
+                | string
+                | number
+                | bigint
+                | boolean
+                | ReactElement<unknown, string | JSXElementConstructor<any>>
+                | Iterable<ReactNode>
+                | ReactPortal
+                | Promise<
+                    | string
+                    | number
+                    | bigint
+                    | boolean
+                    | ReactPortal
+                    | ReactElement<unknown, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | null
+                    | undefined
+                  >
+                | null
+                | undefined;
+              status:
+                | string
+                | number
+                | bigint
+                | boolean
+                | ReactElement<unknown, string | JSXElementConstructor<any>>
+                | Iterable<ReactNode>
+                | ReactPortal
+                | Promise<
+                    | string
+                    | number
+                    | bigint
+                    | boolean
+                    | ReactPortal
+                    | ReactElement<unknown, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | null
+                    | undefined
+                  >
+                | null
+                | undefined;
+            }) => (
+              <tr key={lift.id}>
+                {" "}
+                <td>{lift.name}</td>
+                <td>{lift.status}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </main>
+  );
 }

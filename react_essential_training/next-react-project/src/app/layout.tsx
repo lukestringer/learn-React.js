0696e5cc71002fdf42cda35259ca3898396bc440
Luke Stringer <luke97stringer@gmail.com>
2025-04-15 16:37:51 +1000

    some tailwind css and server component

--- react_essential_training/next-react-project/src/app/layout.tsx
+++ react_essential_training/next-react-project/src/app/layout.tsx
@@ -20,14 +20,17 @@ export const metadata: Metadata = {
 
 function Header() {
   return (
-    <header>
-      <div>
-        <nav>
-          <div>
+    <header className="bg-slate-500 py-4">
+      <div className="container mx-auto px-4">
+        <nav className="flex items-center justify-between">
+          <div className="flex items-center">
             <Link href="/">Snowtooth Mountain</Link>
           </div>
           <div>
             <Link href="/mountain">Mountain info</Link>
+          </div>
+          <div>
+            <Link href="/hotels">Hotels</Link>
           </div>
         </nav>
       </div>

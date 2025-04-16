2c552b7b8df9d556f349053ba43e04b204d8595b
Luke Stringer <luke97stringer@gmail.com>
2025-04-16 17:40:46 +1000

    fetching data then passing data as props

--- react_essential_training/next-react-project/src/app/hotels/page.tsx
+++ react_essential_training/next-react-project/src/app/hotels/page.tsx
@@ -1,8 +1,53 @@
-export default function Page() {
+import { Key } from "react";
+
+async function getData() {
+  const res = await fetch("https://snowtooth-hotel-api.fly.dev");
+  return res.json();
+}
+
+//dynamic component for Hotels page
+function HotelBlock({
+  name,
+  capacity,
+  avgCost,
+}: {
+  name: string;
+  capacity: number;
+  avgCost: number;
+}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>
+        It has room for {capacity} guests and costs ${avgCost} on average.
+      </p>
+    </div>
+  );
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <main>
       <div>
         <h1>Hotel Details</h1>
+        <div>
+          {data.map(
+            (hotel: {
+              id: Key | null | undefined;
+              name: string;
+              capacity: number;
+              avgCost: number;
+            }) => (
+              <HotelBlock
+                key={hotel.id}
+                name={hotel.name}
+                capacity={hotel.capacity}
+                avgCost={hotel.avgCost}
+              />
+            )
+          )}
+        </div>
       </div>
     </main>
   );

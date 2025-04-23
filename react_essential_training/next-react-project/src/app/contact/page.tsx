c416dbd152fda86bc29b533103a3f2e8bb540d66
Luke Stringer <luke97stringer@gmail.com>
2025-04-23 21:49:41 +1000

    building a form

--- react_essential_training/next-react-project/src/app/contact/page.tsx
+++ react_essential_training/next-react-project/src/app/contact/page.tsx
@@ -1,11 +1,42 @@
 export default function Page() {
   return (
-    <div>
-      <p>Contact Us:</p>
-      <ul>
-        <li>email: me@me.com</li>
-        <li>phone: 0412345678</li>
-      </ul>
-    </div>
+    <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md text-black">
+      <h1 className="text-2xl font-bold text-center mb-6">Contact Us:</h1>
+      <form action="" className="space-y-4">
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-white-700"
+          >
+            Email Address
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            className="border-black-300 focus:ring-2 focus:ring-aqua-500 focus:border-aqua-500"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-white-700"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            required
+            rows={4}
+            className="border-black-300 focus:ring-2 focus:ring-aqua-500 focus:border-aqua-500"
+          ></textarea>
+        </div>
+        <button type="submit" className="text-white bg-blue-600 rounded-md p-3">
+          Send Message
+        </button>
+      </form>
+    </main>
   );
 }
